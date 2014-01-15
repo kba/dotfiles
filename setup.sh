d2ebe647c85557bffc050e5b739e62e7a77232cc
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-01-15 19:38:17 +0100

    script to setup zsh and vim

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+dotfiledir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+repodir=$dotfiledir/repo
+cd $dotfiledir
+
+if [[ ! -e $repodir ]];then
+    mkdir $repodir;
+fi
+
+setup_repo() {
+    repo=$1
+    echo "set up $repo"
+    cd $repodir
+    if [[ -e $repo ]];then
+        echo "Repository $repo already exists, skipping";
+    else
+        git clone https://github.com/kba/${repo}.git
+        cd $repo
+        bash setup.sh
+    fi
+    cd $dotfiledir
+}
+
+setup_repo zsh-config
+setup_repo vim-config
