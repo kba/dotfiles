6155058fe254a7428445a5449723125855b70e0b
Konstantin Baierer <unixprog@gmail.com>
2015-10-03 05:36:53 +0200

    better autostart iwth pkill and such

--- misc/autostart.sh
+++ misc/autostart.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-guake&
-fbxkb&
-devilspie2&
+for cmd in $(echo "
+    nm-applet
+    devilspie2
+    fbxkb
+    guake
+    xbindkeys
+    redshift-gtk
+    clipit
+    conky
+    ");do
+    if which $cmd >/dev/null 2>&1; then
+        echo pkill -f "$cmd"
+        pkill -f "$cmd"
+        echo sh -c "$cmd"
+        sh -c "$cmd"&
+    else
+        echo "Not installed: $cmd"
+    fi
+done
 bash $DIR/synaptics.sh&
-conky -c $DIR/conky-right&
-xbindkeys&
-redshift&
-clipit&
 setxkbmap -option grp:alt_space_toggle us,de
 compiz --replace&
--- misc/devilspie2/eclipse.lua
+++ misc/devilspie2/eclipse.lua
@@ -1,4 +1,5 @@
-if (get_application_name()=='java') and not get_window_has_name() then
+if (get_application_name()=='java') and get_window_class() == 'Eclipse' then
+    set_window_name('Eclipse Console')
     set_skip_pager(false)
 end
 
